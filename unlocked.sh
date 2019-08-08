27adcf6a6888ec62aa39eb62024a6cf92a304e4f
Agrim Asthana <dev@agrimasthana.com>
2019-08-08 06:33:37 -0500

    Added cowsay
    added in cowsay for the lulz.

--- unlocked.sh
+++ unlocked.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo 'export HISTCONTROL=ignorespace' >> ~/.bash_profile;
-brew install m-cli;
+brew install m-cli cowsay;
 echo "alias sl='m lock'" >> ~/.bash_profile;
 npm i -g begoo;
+echo "cowsay -f daemon "\""you shouldn't have left your computer unlocked"\"" " >> ~/.bash_profile;
 echo "begoo 'next time please lock your computer' --avatar dog" >> ~/.bash_profile;
