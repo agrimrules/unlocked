186d687a33c0f9f7fa5e7432eccaf352b7426995
Agrim Asthana <dev@agrimasthana.com>
2019-08-08 06:43:08 -0500

    shellcheck fixes

--- unlocked.sh
+++ unlocked.sh
@@ -3,5 +3,5 @@ echo 'export HISTCONTROL=ignorespace' >> ~/.bash_profile;
 brew install m-cli cowsay;
 echo "alias sl='m lock'" >> ~/.bash_profile;
 npm i -g begoo;
-echo "cowsay -f daemon "\""you shouldn't have left your computer unlocked"\"" " >> ~/.bash_profile;
+echo "cowsay -f daemon \"you shouldn't have left your computer unlocked\" " >> ~/.bash_profile;
 echo "begoo 'next time please lock your computer' --avatar dog" >> ~/.bash_profile;
