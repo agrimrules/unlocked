1264b6e768da3b0807dcb015665b3b42e2b670af
Agrim Asthana <dev@agrimasthana.com>
2019-08-19 00:46:36 -0500

    Adding slackcat

--- unlocked.sh
+++ unlocked.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'export HISTCONTROL=ignorespace' >> ~/.bash_profile;
-brew install m-cli cowsay;
+brew install m-cli cowsay slackcat;
 echo "alias sl='m lock'" >> ~/.bash_profile;
 npm i -g begoo;
 echo "cowsay -f daemon \"you shouldn't have left your computer unlocked\" " >> ~/.bash_profile;
