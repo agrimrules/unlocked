0753a1b2436552b6081e84294183994f4ec6047f
Agrim Asthana <dev@agrimasthana.com>
2019-08-26 23:30:24 -0500

    Add chuck norris cli

--- unlocked.sh
+++ unlocked.sh
@@ -3,5 +3,6 @@ echo 'export HISTCONTROL=ignorespace' >> ~/.bash_profile;
 brew install m-cli cowsay slackcat;
 echo "alias sl='m lock'" >> ~/.bash_profile;
 npm i -g begoo;
+pip install norris-cli;
 echo "cowsay -f daemon \"you shouldn't have left your computer unlocked\" " >> ~/.bash_profile;
 echo "begoo 'next time please lock your computer' --avatar dog" >> ~/.bash_profile;
